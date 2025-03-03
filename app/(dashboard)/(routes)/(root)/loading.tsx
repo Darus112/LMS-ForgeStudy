9d0fafa85c1a927832d311befa121a8c5f56a132
Darus112 <edarius123@gmail.com>
2025-03-03 18:02:21 +0200

    small adjustments

--- app/(dashboard)/(routes)/(root)/loading.tsx
+++ app/(dashboard)/(routes)/(root)/loading.tsx
@@ -4,7 +4,7 @@ import { Skeleton } from "@/components/ui/skeleton";
 const Loading = () => {
   return (
     <div className="p-6 space-y-4">
-      <div className="flex space-x-4">
+      <div className="grid grid-cols-1 xl:grid-cols-2 gap-4">
         {Array.from({ length: 2 }).map((_, i) => (
           <Skeleton key={i} className="h-20 w-full rounded-sm" />
         ))}
